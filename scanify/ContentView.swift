b1605792bc9c08616a6f3e5c4b42763bb4482abd
Riley Schnee <rischnee@umich.edu>
2019-10-30 13:50:36 -0400

    Initial file setup

---
+++ scanify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scanify
+//
+//  Created by Riley Schnee on 10/30/19.
+//  Copyright © 2019 Riley Schnee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
